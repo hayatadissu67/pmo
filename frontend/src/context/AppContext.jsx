4450b92c41dcde46e50d5c25e0d52707392cb297
gemechu <gemechujimacs@gmail.com>
2026-07-21 10:02:28 +0300

    structure of register

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -5,8 +5,14 @@ const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
    const [user, setUser] = useState(null);
+
   const login = async (credentials) => {
     const data = await AuthService.login(credentials);
+    setUser(data.user);
+  };
+
+  const register = async (userData) => {
+    const data = await AuthService.register(userData);
     setUser(data.user);
   };
 
@@ -15,7 +21,7 @@ export const AppProvider = ({ children }) => {
   };
 
   return (
-    <AppContext.Provider value={{ user, login, logout }}>
+    <AppContext.Provider value={{ user, login, register, logout }}>
       {children}
     </AppContext.Provider>
   );
--- frontend/src/pages/auth/RegisterPage.jsx
+++ frontend/src/pages/auth/RegisterPage.jsx
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import InputField from "../../components/forms/InputField";
 import Checkbox from "../../components/forms/Checkbox";
 import Button from "../../components/buttons/Button";
-
+import { useAuth } from "../../context/AppContext";
 export default function RegisterPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [agreeTerms, setAgreeTerms] = useState(false);
-
+const { register } = useAuth();
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log({ name, email, password, confirmPassword, agreeTerms });
-   
+    register({ name, email, password });
   };
 
   return (
