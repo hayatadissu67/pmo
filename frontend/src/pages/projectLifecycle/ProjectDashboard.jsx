2b5852ae867af7bc8f7f7b81f20776f1083f93cc
gemechu <gemechujimacs@gmail.com>
2026-07-21 09:41:07 +0300

    I remove loginpage from project dashboardd

--- frontend/src/pages/projectLifecycle/ProjectDashboard.jsx
+++ frontend/src/pages/projectLifecycle/ProjectDashboard.jsx
@@ -3,7 +3,8 @@ export default function ProjectDashboard() {
   
   return (
     <div>
-       <LoginPage/>
+      <h1 className="text-3xl font-bold mb-4">Project Dashboard</h1>
+      <p>Welcome to the Project Dashboard! Here you can manage your projects, tasks, resources, and more.</p> 
     </div>
   )
 }
